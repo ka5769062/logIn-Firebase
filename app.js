2533c3495713d17be2a228820703d691bd259eb0
ka5769062 <75374110+ka5769062@users.noreply.github.com>
2023-06-27 18:22:48 +0500

    login

---
+++ app.js
@@ -0,0 +1,14 @@
+ function myFunction(){
+
+    alert("hello")
+
+}
+
+
+// var kam = document.getElementById('logn')
+// kam.addEventListener("click",function(){
+
+//     alert("hello")
+
+
+// })
